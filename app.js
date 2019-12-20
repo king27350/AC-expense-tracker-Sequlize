0a11c804b4f042e90f1dd0fe80fc9b2fed64bd53
Chris <shian0222@gmail.com>
2019-12-20 16:07:00 +0800

    Add routes and viewsfor register and login pages

--- app.js
+++ app.js
@@ -15,6 +15,27 @@ app.use(methodOverride('_method'))
 app.get('/', (req, res) => {
   res.send('hello world')
 })
+// 認證系統的路由
+// 登入頁面
+app.get('/users/login', (req, res) => {
+  res.render('login')
+})
+// 登入檢查
+app.post('/users/login', (req, res) => {
+  res.send('login')
+})
+// 註冊頁面
+app.get('/users/register', (req, res) => {
+  res.render('register')
+})
+// 註冊檢查
+app.post('/users/register', (req, res) => {
+  res.send('register')
+})
+// 登出
+app.get('/users/logout', (req, res) => {
+  res.send('logout')
+})
 
 app.listen(3000, () => {
   console.log(`App is running on port 3000 !`)
