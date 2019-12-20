0e2e3469ff75d732a36bb91cc81df07105fcab52
Chris <shian0222@gmail.com>
2019-12-20 21:07:23 +0800

    Add home and user routers

--- app.js
+++ app.js
@@ -18,31 +18,9 @@ app.use(methodOverride('_method'))
 app.get('/', (req, res) => {
   res.send('hello world')
 })
-// 認證系統的路由
-// 登入頁面
-app.get('/users/login', (req, res) => {
-  res.render('login')
-})
-// 登入檢查
-app.post('/users/login', (req, res) => {
-  res.send('login')
-})
-// 註冊頁面
-app.get('/users/register', (req, res) => {
-  res.render('register')
-})
-// 註冊檢查
-app.post('/users/register', (req, res) => {
-  User.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  }).then(user => res.redirect('/'))
-})
-// 登出
-app.get('/users/logout', (req, res) => {
-  res.send('logout')
-})
+
+//
+app.use('/users', require('./routes/user'))
 
 app.listen(3000, () => {
   console.log(`App is running on port 3000 !`)
