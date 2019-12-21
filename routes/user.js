8f2f0e4079b407072ab6a22e52a8038b0fad37e9
Chris <shian0222@gmail.com>
2019-12-21 21:28:05 +0800

    fixed bu

--- routes/user.js
+++ routes/user.js
@@ -10,7 +10,7 @@ router.get('/login', (req, res) => {
   res.render('login')
 })
 //登入檢查
-router.post('/login', (req, res) => {
+router.post('/login', (req, res, next) => {
   passport.authenticate('local', {
     successRedirect: '/',
     failureRedirect: '/users/login',
